581a5c71c26174f98b0e9a8150398247fa075988
jluiz-programacao <202503027871@alunos.estacio.br>
2025-02-27 17:51:54 +0000

    teste inicial logica if

--- treino_logica.c
+++ treino_logica.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main (){
+    int numero1, numero2;
+
+    numero1 = 18;
+    numero2 = 5;
+
+    if(numero1 > numero2){
+        printf("Número 1 é maior que número 2\n");
+
+    }
+
+    printf("Fora IF");
+}
